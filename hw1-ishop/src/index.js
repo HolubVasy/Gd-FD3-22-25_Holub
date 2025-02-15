ee587cd4a2efbf02701993a9be6e8405ba12f59e
v.holub <holubvasy@gmail.com>
2025-02-15 21:37:18 +0300

    feat: add functionality

--- hw1-ishop/src/index.js
+++ hw1-ishop/src/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Shop from './components/Shop';
+
+const shopData = {
+  name: "MyShop",
+  products: [
+    {
+      id: 1,
+      name: "Ноутбук",
+      price: 999.99,
+      image: "https://picsum.photos/200/150?random=1",
+      stock: 5
+    },
+    {
+      id: 2,
+      name: "Смартфон",
+      price: 499.99,
+      image: "https://picsum.photos/200/150?random=2",
+      stock: 8
+    },
+    {
+      id: 3,
+      name: "Наушники",
+      price: 99.99,
+      image: "https://picsum.photos/200/150?random=3",
+      stock: 15
+    },
+    {
+      id: 4,
+      name: "Планшет",
+      price: 299.99,
+      image: "https://picsum.photos/200/150?random=4",
+      stock: 3
+    }
+  ]
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Shop name={shopData.name} products={shopData.products} />
   </React.StrictMode>
 );
 
