1d4537dd1ef107edcf23d01dead52290c8119a42
v.holub <holubvasy@gmail.com>
2025-02-24 00:26:07 +0300

    refact: Minor component syntax updates in App and TagsSection

--- hw4-smart-notes/src/App.tsx
+++ hw4-smart-notes/src/App.tsx
@@ -1,16 +1,15 @@
-// src/App.tsx
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import MainContent from './components/MainContent';
 import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
     <Provider store={store}>
       <MainContent />
     </Provider>
   );
-};
+}
 
 export default App;
--- hw4-smart-notes/src/components/Tags/TagsSection.tsx
+++ hw4-smart-notes/src/components/Tags/TagsSection.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import TagComponent from '../Tag/Tag';
 import TagModal from '../Modal/TagModal';
