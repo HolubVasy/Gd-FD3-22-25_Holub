83560b288b9378d202617c08179ece883c0dfe15
v.holub <holubvasy@gmail.com>
2025-03-18 23:46:32 +0300

    feat: Implement Redux store with persistence for auth and search slices

---
+++ home-wiki/src/redux/store.ts
@@ -0,0 +1,36 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import authReducer from './slices/authSlice';
+import articleReducer from './slices/articleSlice';
+import searchReducer from './slices/searchSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth', 'search'], // Only persist auth and search state
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  articles: articleReducer,
+  search: searchReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch; 
